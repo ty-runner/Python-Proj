076ce43bbc9e71fc368fe431f3b1ea30eef799ee
ty-runner <ty.runner02@gmail.com>
2023-07-24 20:49:03 -0700

    Finish

--- YoutubeVideoDownloader/ytdownloader.py
+++ YoutubeVideoDownloader/ytdownloader.py
@@ -4,7 +4,10 @@ from sys import argv
 link = argv[1]
 
 yt = YouTube(link)
-print(yt.title)
+print("Downloading: " + yt.title)
+yd = yt.streams.get_highest_resolution()
+yd.download()
 
-
+#how can this be grown?
+#make it a global script?
 #RUN FROM TERMINAL AS python -m ytdownloader "LINK"
