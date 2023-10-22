5a487b0fa64c3f634261e653049b7b896bde7206
ty-runner <ty.runner02@gmail.com>
2023-10-21 20:35:51 -0400

    Fetches data from trading website

--- Web Scraper/scraper.py
+++ Web Scraper/scraper.py
@@ -3,34 +3,49 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
-url = "https://www.smartinsider.com/politicians/"
+url = "https://www.quiverquant.com/sources/senatetrading"
 response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Specify the file path where you want to save the content
+file_path = "soup_content.html"  # Change this to the desired file path and name
 
+    # Write the content of soup to the specified file
+with open(file_path, "w", encoding="utf-8") as file:
+        file.write(soup.prettify())
+with open('soup_content.html', 'r', encoding='utf-8') as file:
+    html_content = file.read()
 # Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the HTML content of the page using BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Locate the section containing recent trades
-    recent_trades_section = soup.find('section', {'class': 'recent-trades'})
-    
-    if recent_trades_section:
-        # Extract the relevant data
-        trade_items = recent_trades_section.find_all('div', {'class': 'trade-item'})
-        
-        for trade_item in trade_items:
-            # Extract and print the trade information
-            trade_date = trade_item.find('div', {'class': 'trade-date'}).text.strip()
-            trade_description = trade_item.find('div', {'class': 'trade-description'}).text.strip()
-            trade_type = trade_item.find('div', {'class': 'trade-type'}).text.strip()
-            trade_amount = trade_item.find('div', {'class': 'trade-amount'}).text.strip()
-            
-            print(f"Date: {trade_date}")
-            print(f"Description: {trade_description}")
-            print(f"Type: {trade_type}")
-            print(f"Amount: {trade_amount}")
-            print('-' * 40)
-    else:
-        print("Recent trades section not found on the page.")
-else:
-    print("Failed to retrieve the page. Status code:", response.status_code)
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find the table with class 'senate-trading-table'
+table = soup.find('table', class_='senate-trading-table')
+
+# Initialize lists to store the scraped data
+data = []
+
+# Iterate through rows in the table
+for row in table.find_all('tr'):
+    columns = row.find_all('td')
+    if columns:
+        stock = columns[0].text.strip()
+        date_disclosed = columns[1].text.strip()
+        senator = columns[2].text.strip()
+        purchase_sale = columns[3].text.strip()
+        trade_type = columns[4].text.strip()
+        amount = columns[5].text.strip()
+        party = columns[6].text.strip()
+
+        data.append({
+            "Stock": stock,
+            "Date Disclosed": date_disclosed,
+            "Senator": senator,
+            "Purchase / Sale": purchase_sale,
+            "Type": trade_type,
+            "Amount": amount,
+            "Party": party
+        })
+
+# Print the scraped data
+for entry in data:
+    print(entry)
